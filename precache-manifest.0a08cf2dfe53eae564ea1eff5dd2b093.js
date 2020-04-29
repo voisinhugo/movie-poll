14e3385cce4541e4d64f6ba08ef2a98cc2f7debb
Hugo Voisin <hugov@bam.tech>
2020-04-30 01:15:35 +0200

    Updates

---
+++ precache-manifest.0a08cf2dfe53eae564ea1eff5dd2b093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c83d4f1c81fa256496d04889bcd620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55580bc877c3a971a7c1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7662a3e0eab139241d87",
+    "url": "/static/js/2.3c68c012.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c68c012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55580bc877c3a971a7c1",
+    "url": "/static/js/main.0984e37f.chunk.js"
+  },
+  {
+    "revision": "8cfe608e1dc9a57fbae2",
+    "url": "/static/js/runtime-main.da176bb2.js"
+  }
+]);
